285fe9346c22ae1ec0fe954d5f9cf909a69cdb94
Абдула Хизриев <khizriev.abdula31@gmail.com>
2024-02-06 22:43:25 +0300

    init commit

---
+++ src/main/java/org/mizuro/springboot/EmailSpringBootAppApplication.java
@@ -0,0 +1,13 @@
+package org.mizuro.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailSpringBootAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmailSpringBootAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/mizuro/springboot/EmailSpringBootAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mizuro.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailSpringBootAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
